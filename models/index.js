79c519931c7957bc979f6108ada3e781c50eed91
G6-2Flyy <109106713+G6-2Flyy@users.noreply.github.com>
2023-11-01 11:14:33 -0500

    created post model route

--- models/index.js
+++ models/index.js
@@ -1,9 +1,20 @@
 // models/index.js
 const User = require('./user')
+const User = require('./user')
+const Post = require('./post')
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+
+
+module.exports = { User, Post }
 
 
 
 
 
-
-module.exports = { User }
