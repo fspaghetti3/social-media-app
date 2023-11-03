508bf4f8974c4106f4e128fb8b96642d460407a2
G6-2Flyy <109106713+G6-2Flyy@users.noreply.github.com>
2023-11-03 17:53:36 -0500

    complete video play and pause events

---
+++ public/js/video.js
@@ -0,0 +1,21 @@
+document.querySelectorAll('.post-item').forEach((el)=> {
+    el.addEventListener('mouseenter', (event)=> {
+        const video = event.target.querySelector('video')
+        video.play()
+    } )
+    el.addEventListener('mouseout', (event)=> {
+        const video = event.target.querySelector('video')
+        video.pause()
+    })
+})
+
+document.querySelectorAll('video').forEach((el)=> {
+    el.addEventListener('mouseenter', (event)=> {
+        const video = event.target
+        video.play()
+    } )
+    el.addEventListener('mouseout', (event)=> {
+        const video = event.target
+        video.play()
+    })
+})
